8370a63878cdf10b3a7be1668a6da4ca5109bc12
Sharaaf Nazeer <Sharaaf.Nazeer@author-it.com>
2021-07-09 18:08:43 +0530

    driving updated

--- controllers/drivingController.js
+++ controllers/drivingController.js
@@ -135,11 +135,116 @@ const updateDriverOkay = async (req, res, next) => {
             await db.collection('usertracking').doc(req.userId).update(updateData);
         }
 
-        if (req.body.vibrationValue > 0) {
-            if (req.body.accelerometerValue > accidentSettings.accelerometerThreshold) {
-                newUserTracking.setSuspecious(true);
-                object = newUserTracking.getObject();
-                await db.collection('usertracking').doc(req.body.userId).update(object);
+        if(accidentOccured) {
+            try {
+                const userCircles = await db.collection('circles').where("status", '==', 1).get();
+        
+                const actualCircles = [];
+                userCircles.forEach((doc) => {
+                    if (doc.data().user === req.userId || doc.data().circleUser === req.userId) {
+                        actualCircles.push({
+                            ...doc.data(),
+                            id: doc.id,
+                        });
+                    }
+                });
+                const circleResponse = await Promise.all(actualCircles.map(async (userCircleDoc) => {
+        
+                    try {
+                    
+                        let users = await db.collection('users').get();
+                        const actualUsers = [];
+                        users.forEach(async (doc) => {
+                            if (doc.id === userCircleDoc.user || doc.id === userCircleDoc.circleUser) {
+                                actualUsers.push({
+                                    ...doc.data(),
+                                    id: doc.id,
+                                });
+                            }                
+                        })
+                        
+                        return actualUsers.map(user => {                    
+                            return {
+                                id: user.id,
+                                firebaseToken: user.firebaseToken,
+                                name: user.firstName + " " + user.lastName,
+                                phone: user.phone,
+                                type: userCircleDoc.type,
+                                lastLocation: user.lastLocation
+                            }
+                        });
+        
+                    } catch (error) {
+                        return jsonResponse(res, 500, errorRes(error))
+                    }
+        
+                }));
+        
+                const responses = [];
+                circleResponse.forEach(async (userDocs) => {
+                    userDocs.forEach(async (user) => {
+                        if ((user.id != req.userId) && user.firebaseToken) {
+                            responses.push(user);
+                        }                   
+                    })
+                });
+        
+                const finalResponses = await Promise.all(responses.map(async (res) => {
+        
+                    try {            
+                        const vehicle = db.collection('vehicles').where("user", '==', res.id).where("status", '==', 1);
+                        const availableVehicle = await vehicle.get();                        
+                        if (!availableVehicle.empty) {
+                            const actualVehicle = availableVehicle.docs[0].data();
+                            return {
+                                ...res,
+                                vehicle: actualVehicle.brand + " " + actualVehicle.model,
+                                vehicleNo: actualVehicle.vehicleNo
+                            };
+                        } else {
+                            return {
+                                ...res
+                            };
+                        }
+        
+                    } catch (error) {
+                        return jsonResponse(res, 500, errorRes(error))
+                    }
+                }));
+        
+                finalResponses.forEach(async (element) => {
+                    const fireResponse = await admin.messaging().sendToDevice(
+                        [element.firebaseToken], // ['token_1', 'token_2', ...]
+                        {
+                            notification: {
+                               title: "Accident Suspecious Warning!",
+                               body: "We suspect that one of your family or friend is under danger. Please kindly check whether he/she is okay at the moment",
+                            },
+                            data: {
+                                name: element.name,
+                                phone: element.phone,
+                                type: element.type,
+                                vehicle: element.vehicle,
+                                vehicleNo: element.vehicleNo,
+                                lastLocationLat: element.lastLocation?.lat + "", 
+                                lastLocationLong: element.lastLocation?.long + ""
+                            }
+                        },
+                        {
+                          // Required for background/quit data-only messages on iOS
+                          contentAvailable: true,
+                          // Required for background/quit data-only messages on Android
+                          priority: "high",
+                          timeToLive: 60*60*24
+                        }
+                      );
+        
+                    console.log(fireResponse);
+                });
+                return jsonResponse(res, 200, successRes(finalResponses));
+            } catch (errror) {
+                console.log(error)
+                return jsonResponse(res, 500, errorRes(error))
             }
         }
 
